d3af4855f89207a09fe3a4224e60ef2abbc93d21
ghmessias <ghmessias@estudante.ufscar.br>
2024-01-29 15:51:37 +0000

    rewiring_graphs

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import argparse
 from utils.rewiring import rewiring
 from torch_geometric.datasets import Planetoid
-from torch_geometric.utils import to_networkx
+from torch_geometric.utils import to_networkx, from_networkx
 from graph_generators.generator import generator
 from models.RGCN_model import RGCN_model
 from models.GCN_model import GCN_model
@@ -9,6 +9,10 @@ from models.MLP_model import MLP_model
 import torch
 from torch_geometric.nn import GAE
 from utils.training import train
+from utils.evaluate import negative_inference, evaluate_model
+import json
+import networkx as nx
+import random
 
 parser = argparse.ArgumentParser()
 
@@ -38,6 +42,8 @@ if __name__ == '__main__':
 
     dataset = Planetoid(root = 'datasets', name = args.name)
     data = dataset[0]
+    random.seed(100)
+    torch.manual_seed(100)
 
     if args.generator:
         L_range = [int(x) for x in args.L_range.split()]
@@ -49,28 +55,55 @@ if __name__ == '__main__':
     if args.training:
         models = list()
         in_channels = data.x.shape[0]
+        # Arrumar isso pro args
         hidden_channels = 64
         out_channels = 16
+
+        # Definindo os modelos e carregando os grafos
+
+        # opening json with informations about the graph and positive nodes
+        json_path = 'rewiring_results/positive_nodes/positive_nodes.json'
+
+        with open(json_path, 'r') as file:
+            positive_dict = json.load(file)
         model_losses = dict()
+
+        P = positive_dict[f'L_{args.L}_P_{args.P_rate}_ro_{args.ro}_sample_{args.samples}']
         
-        # if 'RGCN' in args.model:
-        #     RGCN_encoder = RGCN_model(data.x.shape[1], hidden_channels, out_channels, L = args.L)
-        #     RGCN_decoder = MLP_model(out_channels, hidden_channels, data.x.shape[1])
-        #     GAE_RGCN = GAE(encoder = RGCN_encoder, decoder = RGCN_decoder)
-        #     optimizer_RGCN = torch.optim.Adam(GAE_RGCN.parameters(), lr = args.lr, weight_decay = args.L2norm)
-        #     GAE_RGCN.float()
+        if 'RGCN' in args.model:
+            # In case of rewiring, we need to read the .graphml objects to create a list of graphs
+            graphs = list()
+            for i in range(args.L - 1):
+                aux_graph = nx.read_graphml(f'rewiring_results/graphs/graph_L_{args.L}_P_{args.P_rate}_ro_{args.ro}_sample_{args.samples}_{i}.graphml')
+                pyg_graph = from_networkx(aux_graph)
+                graphs.append(pyg_graph)
 
-        #     losses_RGCN = train(data, GAE_RGCN, optimizer_RGCN, args.epochs, args.L, args.ro, args.P_rate, args.samples, rewiring=True)
-        #     model_losses['loss RGCN'] = losses_RGCN
+            graphs = [data] + graphs
 
-        if 'GCN' in args.model:
-            GCN_encoder = GCN_model(data.x.shape[1], hidden_channels, out_channels)
-            GCN_decoder = MLP_model(out_channels, hidden_channels, data.x.shape[1])
-            GAE_GCN = GAE(encoder = GCN_encoder, decoder = GCN_decoder)
-            optimizer_GCN = torch.optim.Adam(GAE_GCN.parameters(), lr = args.lr, weight_decay = args.L2norm)
+            RGCN_encoder = RGCN_model(data.x.shape[1], hidden_channels, out_channels, L = args.L)
+            RGCN_decoder = MLP_model(out_channels, hidden_channels, data.x.shape[1])
+            GAE_RGCN = GAE(encoder = RGCN_encoder, decoder = RGCN_decoder)
+            optimizer_RGCN = torch.optim.Adam(GAE_RGCN.parameters(), lr = args.lr, weight_decay = args.L2norm)
+            GAE_RGCN.float()
+
+            losses_RGCN = train(model = GAE_RGCN,
+                                optimizer=optimizer_RGCN,
+                                epochs = args.epochs,
+                                P = P,
+                                rewiring = True,
+                                graph_list=graphs)
+            model_losses['loss RGCN'] = losses_RGCN
+
+        # if 'GCN' in args.model:
+        #     GCN_encoder = GCN_model(data.x.shape[1], hidden_channels, out_channels)
+        #     GCN_decoder = MLP_model(out_channels, hidden_channels, data.x.shape[1])
+        #     GAE_GCN = GAE(encoder = GCN_encoder, decoder = GCN_decoder)
+        #     optimizer_GCN = torch.optim.Adam(GAE_GCN.parameters(), lr = args.lr, weight_decay = args.L2norm)
             
-            losses_GCN = train(data, GAE_GCN, optimizer_GCN, args.epochs, args.L, args.ro, args.P_rate, args.samples, rewiring=False)
-            model_losses['loss GCN'] = losses_GCN
+        #     losses_GCN = train(data, GAE_GCN, optimizer_GCN, args.epochs, args.L, args.ro, args.P_rate, args.samples, rewiring=False)
+        #     model_losses['loss GCN'] = losses_GCN
+
+        # negatives = negative_inference(GAE_RGCN, )
 
 
         
--- utils/training.py
+++ utils/training.py
@@ -6,54 +6,55 @@ from torch_geometric.datasets import Planetoid
 import torch.nn.functional as F
 from torch_geometric.utils import from_networkx
 
-def train(data, model, optimizer, epochs, L, ro, P_rate, samples, rewiring = True):
-    random.seed(100)
-    torch.manual_seed(100)
+def train(model, optimizer, epochs, P, rewiring = True, graph_list = None, graph = None):
 
-    model.train()
-    
-    # opening json with informations about the graph and positive nodes
-    json_path = 'rewiring_results/positive_nodes/positive_nodes.json'
-
-    with open(json_path, 'r') as file:
-        positive_dict = json.load(file)
-
-    for sample in range(samples):
-        P = positive_dict[f'L_{L}_P_{P_rate}_ro_{ro}_sample_{sample}']
-
-        graphs = list()
-        
-        if rewiring:
-            # In case of rewiring, we need to read the .graphml objects to create a list of graphs
-            for i in range(L - 1):
-                aux_graph = nx.read_graphml(f'rewiring_results/graphs/graph_L_{L}_P_{P_rate}_ro_{ro}_sample_{sample}_{i}.graphml')
-                pyg_graph = from_networkx(aux_graph)
-                graphs.append(pyg_graph)
-            graphs = [data] + graphs
-        else:
-            # In case of not using rewiring, the list of graphs has only a single graph
-            graphs = [data]
-
+    if rewiring:
         # mask for training
-        mask = torch.zeros(graphs[0].x.shape[0], dtype = torch.bool)
+        mask = torch.zeros(graph_list[0].x.shape[0], dtype = torch.bool)
 
         for element in P:
             mask[element] = True
 
         losses = list()
 
+        model.train()
         for epoch in range(epochs):
             optimizer.zero_grad()
-            H_L = model.encode(graphs[0].x.float(), graphs)
+            H_L = model.encode(graph_list[0].x.float(), graph_list)
             out = model.decode(H_L)
 
-            loss = F.binary_cross_entropy(out, graphs[0].x.float())
+            loss = F.binary_cross_entropy(out, graph_list[0].x.float())
             print(f'epoch: {epoch} | loss: {loss.item()}')
             loss.backward()
             optimizer.step()
 
             losses.append(loss)
         return losses
+
+        
+    else:
+        # mask for training
+        mask = torch.zeros(graph.x.shape[0], dtype = torch.bool)
+
+        for element in P:
+            mask[element] = True
+
+        losses = list()
+
+        model.train()
+        for epoch in range(epochs):
+            optimizer.zero_grad()
+            H_L = model.encode(graph.x.float(), graph)
+            out = model.decode(H_L)
+
+            loss = F.binary_cross_entropy(out, graph.x.float())
+            print(f'epoch: {epoch} | loss: {loss.item()}')
+            loss.backward()
+            optimizer.step()
+
+            losses.append(loss)
+
+            return losses
 
 
 
